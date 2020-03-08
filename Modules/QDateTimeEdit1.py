150a2e98080bac56f6ed83c72041d984866b7e27
DezeZhao <1360536767@qq.com>
2020-03-08 13:31:00 +0800

    QDateTimeEdit

--- Modules/QDateTimeEdit1.py
+++ Modules/QDateTimeEdit1.py
@@ -7,6 +7,11 @@
 """
 不同风格日期设置
 QDateTimeEdit
+
+SIGNAL:
+dateChanged
+timeChanged
+datetimeChanged
 """
 import sys
 
@@ -20,27 +25,33 @@ class QDateTimeEdit1(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.resize(500, 500)
-        self.setWindowTitle('不同风格日期')
+        self.resize(350, 500)
+        self.setWindowTitle('不同风格日期时间')
 
         layout = QVBoxLayout()
+
         dateTimeEdit1 = QDateTimeEdit()
         dateTimeEdit2 = QDateTimeEdit(QDateTime.currentDateTime())
+        # 设置日期最大和最小值
         dateTimeEdit1.setMinimumDate(QDate.currentDate().addDays(-365))
         dateTimeEdit1.setMaximumDate(QDate.currentDate().addDays(365))
+
         self.dateTimeEdit=dateTimeEdit1
 
         dateTimeEdit2.setCalendarPopup(True)  # 上下箭头调整日期变为下拉
 
+        #分别显示日期和时间
         dateEdit = QDateTimeEdit(QDate.currentDate())
         timeEdit = QDateTimeEdit(QTime.currentTime())
 
+        # 设置不同的日期时间显示格式
         dateTimeEdit1.setDisplayFormat("yyyy-MM-dd HH:mm:ss")
         dateTimeEdit2.setDisplayFormat("yyyy/MM/dd HH-mm-ss")
 
         dateEdit.setDisplayFormat("yyyy.MM.dd")
         timeEdit.setDisplayFormat("HH:mm:ss")
 
+        # 槽函数
         dateTimeEdit1.dateChanged.connect(self.onDateChange)
         dateTimeEdit1.timeChanged.connect(self.onTimeChange)
         dateTimeEdit1.dateTimeChanged.connect(self.onDateTimeChange)
@@ -52,6 +63,7 @@ class QDateTimeEdit1(QWidget):
         self.btn=QPushButton('获取日期和时间')
         self.btn.clicked.connect(self.onButtonClick)
         layout.addWidget(self.btn)
+
         self.setLayout(layout)
 
     def onDateChange(self,date):
@@ -64,9 +76,9 @@ class QDateTimeEdit1(QWidget):
         print(datetime)
 
     def onButtonClick(self):
-        dateTime= self.dateTimeEdit.dateTime()
+        dateTime= self.dateTimeEdit.dateTiem()
         print(dateTime)
-        #最大日期
+        #最大/小日期(时间)
         print(self.dateTimeEdit.maximumDate())
         print(self.dateTimeEdit.maximumDateTime())
         print(self.dateTimeEdit.minimumDate())
