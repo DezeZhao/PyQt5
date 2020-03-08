745a50db515ea39b856f37830111ee263f98b18d
DezeZhao <1360536767@qq.com>
2020-03-09 00:14:55 +0800

    PyQt5和web的交互技术 同时使用python和web技术混合开发 同时使用python JavaScript html5 css3

--- Modules/AutoCloseWindow.py
+++ Modules/AutoCloseWindow.py
@@ -5,9 +5,8 @@
 # @Author  : DezeZhao
 import sys
 
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QLabel, QApplication
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
