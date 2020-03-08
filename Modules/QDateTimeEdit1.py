d5fdbfc55d6d8ca95c3258831c7bf12d15ed4914
DezeZhao <1360536767@qq.com>
2020-03-08 13:15:07 +0800

    QDateTimeEdit

--- Modules/QDateTimeEdit1.py
+++ Modules/QDateTimeEdit1.py
@@ -11,8 +11,8 @@ QDateTimeEdit
 import sys
 
 from PyQt5.QtCore import *
-from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+
 
 class QDateTimeEdit1(QWidget):
     def __init__(self):
@@ -20,12 +20,17 @@ class QDateTimeEdit1(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.resize(500,500)
+        self.resize(500, 500)
         self.setWindowTitle('不同风格日期')
 
         layout = QVBoxLayout()
-        dateTimeEdit1=QDateTimeEdit()
-        dateTimeEdit2=QDateTimeEdit()
+        dateTimeEdit1 = QDateTimeEdit()
+        dateTimeEdit2 = QDateTimeEdit(QDateTime.currentDateTime())
+        dateTimeEdit1.setMinimumDate(QDate.currentDate().addDays(-365))
+        dateTimeEdit1.setMaximumDate(QDate.currentDate().addDays(365))
+        self.dateTimeEdit=dateTimeEdit1
+
+        dateTimeEdit2.setCalendarPopup(True)  # 上下箭头调整日期变为下拉
 
         dateEdit = QDateTimeEdit(QDate.currentDate())
         timeEdit = QDateTimeEdit(QTime.currentTime())
@@ -36,16 +41,39 @@ class QDateTimeEdit1(QWidget):
         dateEdit.setDisplayFormat("yyyy.MM.dd")
         timeEdit.setDisplayFormat("HH:mm:ss")
 
+        dateTimeEdit1.dateChanged.connect(self.onDateChange)
+        dateTimeEdit1.timeChanged.connect(self.onTimeChange)
+        dateTimeEdit1.dateTimeChanged.connect(self.onDateTimeChange)
+
         layout.addWidget(dateTimeEdit1)
         layout.addWidget(dateTimeEdit2)
         layout.addWidget(dateEdit)
         layout.addWidget(timeEdit)
-
+        self.btn=QPushButton('获取日期和时间')
+        self.btn.clicked.connect(self.onButtonClick)
+        layout.addWidget(self.btn)
         self.setLayout(layout)
+
+    def onDateChange(self,date):
+        print(date)
+
+    def onTimeChange(self,time):
+        print(time)
+
+    def onDateTimeChange(self,datetime):
+        print(datetime)
+
+    def onButtonClick(self):
+        dateTime= self.dateTimeEdit.dateTime()
+        print(dateTime)
+        #最大日期
+        print(self.dateTimeEdit.maximumDate())
+        print(self.dateTimeEdit.maximumDateTime())
+        print(self.dateTimeEdit.minimumDate())
+        print(self.dateTimeEdit.minimumDateTime())
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main = QDateTimeEdit1()
     main.show()
     sys.exit(app.exec_())
-
